eda7df29c1d0a1028ce15dcb8f6e269fc6a97d95
saidnahum <saidnahum@gmail.com>
2021-07-16 18:05:52 -0500

    update 16/07/2021

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="home">
-    
+    <BlogPost :post="welcomeScreen"/>
+    <BlogPost :post="post" v-for="(post, index) in sampleBlobPost" :key="index"/>
   </div>
 </template>
 
 <script>
+import BlogPost from '../components/BlogPost'
 export default {
   name: "Home",
-  components: {},
+  components: {BlogPost},
+  data(){
+    return {
+      welcomeScreen: {
+        title: "Bienvenidos!",
+        blogPost: "Bienvenidos al blog de PixelWeb, un blog donde se tocan los temas mas actuales y sobresalientes del ámbito del diseño/desarrollo web. Registrate ahora para no perderte de los mejores temas de programación y desarrollo tecnológico",
+        welcomeScreen: true,
+        photo: "coding"
+      },
+      sampleBlobPost: [
+        {
+          title: "Título del post 1",
+          blogHTML: "HTML del post",
+          blogCoverPhoto: "beautiful-stories"
+        },
+        {
+          title: "Título del post 2",
+          blogHTML: "HTML del post 2",
+          blogCoverPhoto: "designed-for-everyone"
+        }
+      ]
+    }
+  }
 };
 </script>
